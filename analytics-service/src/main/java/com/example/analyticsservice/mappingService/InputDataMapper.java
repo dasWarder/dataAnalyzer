b10152dc78663083d83aaba0e9fcad3a77854e92
dasWarder <Al.94_94@mail.ru>
2022-01-19 18:18:08 +0300

    Create config server and configure it for connecting with the private Git repo; move port and eureka config for the microservices to config repository on git

--- analytics-service/src/main/java/com/example/analyticsservice/mappingService/InputDataMapper.java
+++ analytics-service/src/main/java/com/example/analyticsservice/mappingService/InputDataMapper.java
@@ -7,18 +7,19 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
+
 import java.time.LocalDateTime;
 
 @Mapper
 public interface InputDataMapper {
 
-  @Mapping(target = "id", ignore = true)
-  @Mapping(target = "dataId", source = "id")
-  @Mapping(target = "creatingDate", source = "date")
-  AnalysisData inputDataToAnalysisData(InputData inputData);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "dataId", source = "id")
+    @Mapping(target = "creatingDate", source = "date")
+    AnalysisData inputDataToAnalysisData(InputData inputData);
 
-  @AfterMapping
-  default void setProcessingDate(@MappingTarget AnalysisData.AnalysisDataBuilder dataBuilder) {
-    dataBuilder.processingDate(LocalDateTime.now());
-  }
+    @AfterMapping
+    default void setProcessingDate(@MappingTarget AnalysisData.AnalysisDataBuilder dataBuilder) {
+        dataBuilder.processingDate(LocalDateTime.now());
+    }
 }
